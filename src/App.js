7a2cf76cc348bccfcf2a84cb9023c1e07a8efa94
WalbertJr <54550066+WalbertJr@users.noreply.github.com>
2020-07-05 16:17:00 -0600

    Proyecto Casi Finalizado

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
+import 'bulma/css/bulma.css';
+import { Detail } from './pages/Detail';
+import { Home } from './pages/Home';
+import { NotFound } from './pages/NotFound';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route exact path='/detail/:id' component={Detail}/>
+          <Route component={NotFound}/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { MoviesList } from '../components/MoviesList';
+import { Title } from '../components/Title';
+import { SearchForm } from '../components/SearchForm';
+
+export class Home extends Component{
+    state = { usedSearch: false ,results: []}
+    _handleResults = (results) => {
+        this.setState({results, usedSearch: true})
+      }
+    
+      _renderResults = () => {
+        return this.state.results.length === 0 
+          ? <p>Sorry! Results no found</p>
+          : <MoviesList movies={this.state.results}/>
+      }
+
+    render() {
+      return (
+        <div>
+            <Title>Search Movies</Title>
+            <div className="SearchForm-wrapper">
+            <SearchForm onResults={this._handleResults}/>
+            </div>
+            {this.state.usedSearch
+            ? this._renderResults()
+            : <small>Use the form to search a movie</small>
+            }
+      </div>
+      )
+    };
+}
